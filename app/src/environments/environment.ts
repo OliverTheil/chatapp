cb2a0a90e1bf865daa8b78f9a0f8422aaa1342ad
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-03-27 14:30:46 +0200

    000 - Angular and Firebase

---
+++ app/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'chatapp-72bc7',
+    appId: '1:125082109875:web:0721c62f6e85899e4e6dc6',
+    storageBucket: 'chatapp-72bc7.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAwJkOvl_0gtoezo9XPonM_Ev3n6cpJGik',
+    authDomain: 'chatapp-72bc7.firebaseapp.com',
+    messagingSenderId: '125082109875',
+  },
+  production: false,
+};
