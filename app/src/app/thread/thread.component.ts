3a6fea5a602eb0b7b6c6e45e1d08475b78c49991
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-03-31 16:59:57 +0200

    design rework and fixes

--- app/src/app/thread/thread.component.ts
+++ app/src/app/thread/thread.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../data.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-thread',
   templateUrl: './thread.component.html',
-  styleUrls: ['./thread.component.scss']
+  styleUrls: ['./thread.component.scss'],
 })
 export class ThreadComponent implements OnInit {
+  openState: boolean = false;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private data: DataService) {}
 
   ngOnInit(): void {
+    this.subscription = this.data.currentState.subscribe(
+      (openState) => (this.openState = openState)
+    );
   }
 
+  changeState() {
+    if (!this.openState) {
+      this.data.toggleOpen(true);
+    } else if (this.openState) {
+      this.data.toggleOpen(false);
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
